9b31486e404920a2e647d46cd3024d972d306f7f
mahoon13 <tbhmahoon@gmail.com>
2023-02-17 09:58:04 +0330

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+
+import MobileDeviderImage from "/pattern-divider-mobile.svg";
+import DesktopDeviderImage from "/pattern-divider-desktop.svg";
+import DiceIcon from "/icon-dice.svg";
+
+function App() {
+  const [advice, setAdvice] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function loadRandomAdvice() {
+    if (isLoading) return false;
+
+    setIsLoading(true);
+    const res = await fetch("https://api.adviceslip.com/advice").then((res) =>
+      res.json()
+    );
+
+    setAdvice(res.slip);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    loadRandomAdvice();
+  }, []);
+
+  return (
+    <div className="bg-dark-blue w-full h-screen flex justify-center items-center">
+      {advice ? (
+        <div className="bg-dark-grayish-blue text-center w-[90%] max-w-[550px] pt-[2.5rem] sm:pt-[3rem] pb-[4rem] sm:pb-[5rem] px-[1.5rem] sm:px-[2rem] flex flex-col gap-[1.5rem] rounded-xl relative">
+          <p className="uppercase text-neon-green tracking-[0.3rem] font-manrope text-sm">
+            Advice #{advice.id}
+          </p>
+          <p className="text-[28px] text-light-cyan font-manrope-bold">
+            “{advice.advice}”
+          </p>
+
+          <img src={MobileDeviderImage} className="sm:hidden" />
+          <img src={DesktopDeviderImage} className="hidden sm:block" />
+
+          <div
+            className={`absolute bottom-[-40px] left-[calc(50%_-_40px)] w-[80px] h-[80px] bg-neon-green rounded-full flex justify-center items-center cursor-pointer select-none hover:shadow-neon-green hover:shadow-[0_0_20px_0] transition-shadow ${
+              isLoading && "animate-spin"
+            }`}
+            onClick={() => loadRandomAdvice()}
+          >
+            <img src={DiceIcon} className="w-[2rem]" />
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "light-cyan": "hsl(193, 38%, 86%)",
+        "neon-green": "hsl(150, 100%, 66%)",
+        "grayish-blue": "hsl(217, 19%, 38%)",
+        "dark-grayish-blue": "hsl(217, 19%, 24%)",
+        "dark-blue": "hsl(218, 23%, 16%)",
+      },
+      fontFamily: {
+        manrope: ["Manrope", "system-ui", "sans-serif"],
+        "manrope-bold": ["Manrope Bold", "system-ui", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
